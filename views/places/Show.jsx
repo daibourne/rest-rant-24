bec5723996a1751f7f1686af0aa04b30d3aa115c
Daijon Bourne <Daijonbourne1005@gmail.com>
2024-02-21 22:35:43 -0800

    Fix comments

--- views/places/Show.jsx
+++ views/places/Show.jsx
@@ -67,6 +67,43 @@ const Show = ({ place }) => {
                     </div>
                     <h2>Comments</h2>
                     {comments}
+                    <form action={`/places/${place.id}/comments`} method='POST'>
+                    <div className='mb-3'>
+                        <label htmlFor='author'>Author</label>
+                        <input type='text' name='author' id='author' className='form-control' />
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor='rant'>
+                            Rant{' '}
+                            <input type='checkbox' name='rant' id='rant' className='checkbox' />
+                        </label>
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor='stars'>Star Rating</label>
+                        <input
+                            type='number'
+                            step={0.5}
+                            name='stars'
+                            id='stars'
+                            className='form-control'
+                        />
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor='content'>Content</label>
+                        <input
+                            type='text'
+                            name='content'
+                            id='content'
+                            className='form-control'
+                            required
+                        />
+                    </div>
+                    <div className='mb-3'>
+                        <button type='submit' className='btn btn-primary'>
+                            <i className='bi bi-plus-circle-fill'></i> Add Comment
+                        </button>
+                    </div>
+                </form>
                 </div>
             </main>
         </Default>
